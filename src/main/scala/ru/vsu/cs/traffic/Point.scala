c80d1566f6e53123de73b4a23a9625e858549cc9
Sergey Savvin <sav1ch@hotmail.com>
2014-12-10 17:49:56 +0300

    Add == method to Point class
    Method returns whether two points are equal

--- src/main/scala/ru/vsu/cs/traffic/Point.scala
+++ src/main/scala/ru/vsu/cs/traffic/Point.scala
@@ -4,5 +4,9 @@ class Point(val x: Double, val y: Double) {
 
   def --(other: Point): Double = math.sqrt(math.pow(x, 2) + math.pow(y, 2))
 
+  def ==(other: Point): Boolean = (x == other.x) && (y == other.y)
+
   override def toString = "(" + x + ", " + y + ")"
+
+
 }
