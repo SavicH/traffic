69582713f7426ce6610bdbd14b9bc3f0ce5dd07c
Sergey Savvin <sav1ch@hotmail.com>
2014-12-10 18:05:39 +0300

    Fix distance method (operator --) in Point class

--- src/main/scala/ru/vsu/cs/traffic/Point.scala
+++ src/main/scala/ru/vsu/cs/traffic/Point.scala
@@ -2,7 +2,8 @@ package ru.vsu.cs.traffic
 
 class Point(val x: Double, val y: Double) {
 
-  def --(other: Point): Double = math.sqrt(math.pow(x, 2) + math.pow(y, 2))
+  def --(other: Point): Double =
+    math.sqrt(math.pow((x - other.x), 2) + math.pow((y - other.y), 2))
 
   def ==(other: Point): Boolean = (x == other.x) && (y == other.y)
 
