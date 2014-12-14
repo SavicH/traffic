4f6bac3891276520290e32b97b4d7ad8673a4227
Sergey Savvin <sav1ch@hotmail.com>
2014-12-14 18:32:31 +0300

    Move implementation of TrafficFlow trait to file that contains this trait

--- src/main/scala/ru/vsu/cs/traffic/TrafficFlow.scala
+++ src/main/scala/ru/vsu/cs/traffic/TrafficFlow.scala
@@ -1,5 +1,7 @@
 package ru.vsu.cs.traffic
 
+import scala.collection.mutable
+
 trait TrafficFlow {
 
   def start: Point
@@ -16,3 +18,49 @@ trait TrafficFlow {
 
   def spawn()
 }
+
+object TrafficFlow {
+  def apply(start: Point, end: Point, lanes: Int, probability: Double): TrafficFlow =
+    new TrafficFlowImpl(start, end, lanes, probability)
+
+  private class TrafficFlowImpl
+   (private val _start: Point,
+    private val _end: Point,
+    private val _lanes: Int,
+    private val probability: Double) extends TrafficFlow {
+
+    var _vehicles = mutable.MutableList[Vehicle]()
+
+    var _neighbour: TrafficFlow = null
+
+    private def this(start: Point, end: Point, lanes: Int, probability: Double, neighbour: TrafficFlow) = {
+      this(start, end, lanes, probability)
+      _neighbour = neighbour
+    }
+
+    def createNeighbour: TrafficFlow = {
+      if (_neighbour != null) {
+        throw new IllegalStateException("Neighbour can be created only once")
+      }
+      _neighbour = new TrafficFlowImpl(_end, _start, lanes, probability, this)
+      _neighbour
+    }
+
+    override def start: Point = _start
+
+    override def end: Point = _end
+
+    override def lanes: Int = _lanes
+
+    override def neighbour: TrafficFlow = _neighbour
+
+    override def vehicles: Seq[Vehicle] = _vehicles.toList
+
+    override def spawn() = {
+      //todo
+    }
+
+  }
+
+}
+
