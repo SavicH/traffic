4dc5b9955000dddd12f3ec3d4231fa9b01b8709c
Sergey Savvin <sav1ch@hotmail.com>
2014-12-14 18:48:24 +0300

    Add isOneWay: Boolean parameter to TrafficFlowImpl constructor
    Redundant createNeighbour() method was deleted. Creation of neighbour happens during initialization

--- src/main/scala/ru/vsu/cs/traffic/TrafficFlow.scala
+++ src/main/scala/ru/vsu/cs/traffic/TrafficFlow.scala
@@ -20,30 +20,23 @@ trait TrafficFlow {
 }
 
 object TrafficFlow {
-  def apply(start: Point, end: Point, lanes: Int, probability: Double): TrafficFlow =
-    new TrafficFlowImpl(start, end, lanes, probability)
+  def apply(start: Point, end: Point, lanes: Int, isOneWay: Boolean, probability: Double): TrafficFlow =
+    new TrafficFlowImpl(start, end, lanes, isOneWay, probability)
 
   private class TrafficFlowImpl
    (private val _start: Point,
     private val _end: Point,
     private val _lanes: Int,
+    _isOneWay: Boolean,
     private val probability: Double) extends TrafficFlow {
 
     var _vehicles = mutable.MutableList[Vehicle]()
 
-    var _neighbour: TrafficFlow = null
+    var _neighbour: TrafficFlow = if (_isOneWay) null else new TrafficFlowImpl(_end, _start, lanes, probability, this)
 
     private def this(start: Point, end: Point, lanes: Int, probability: Double, neighbour: TrafficFlow) = {
       this(start, end, lanes, probability)
       _neighbour = neighbour
-    }
-
-    def createNeighbour: TrafficFlow = {
-      if (_neighbour != null) {
-        throw new IllegalStateException("Neighbour can be created only once")
-      }
-      _neighbour = new TrafficFlowImpl(_end, _start, lanes, probability, this)
-      _neighbour
     }
 
     override def start: Point = _start
