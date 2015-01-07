e89d86b928aeb83501c3e22c50a8279853f5ddc7
Sergey Savvin <sav1ch@hotmail.com>
2015-01-07 14:03:10 +0300

    Implement TrafficLight trait

--- src/main/scala/ru/vsu/cs/traffic/TrafficLight.scala
+++ src/main/scala/ru/vsu/cs/traffic/TrafficLight.scala
@@ -1,5 +1,7 @@
 package ru.vsu.cs.traffic
 
+import ru.vsu.cs.traffic.Direction._
+
 trait TrafficLight {
 
   sealed trait Color
@@ -11,9 +13,13 @@ trait TrafficLight {
     case object NONE extends Color
   }
 
+  def intersection: Intersection
+
+  def opposite: TrafficLight
+
   def trafficFlows: Seq[TrafficFlow]
 
-  def trafficFLow(direction: Direction)
+  def trafficFlow(direction: Direction): TrafficFlow
 
   def isControlled: Boolean
 
@@ -26,3 +32,38 @@ trait TrafficLight {
   def extendColor(delta: Double)
 
 }
+
+object TrafficLight {
+
+  def apply(trafficFlows: Map[Direction, TrafficFlow], intersection: Intersection) = {
+    new TrafficLightImpl(trafficFlows, intersection)
+  }
+
+  private class TrafficLightImpl
+  (private val _trafficFlows: Map[Direction, TrafficFlow],
+   private val _intersection: Intersection)
+  extends TrafficLight {
+
+    private val _opposite = intersection.trafficLights.filter(_.trafficFlow(BACK) == _trafficFlows(FORWARD)).head
+
+    override def intersection: Intersection = _intersection
+    
+    override def opposite: TrafficLight = _opposite
+
+    override def isControlled: Boolean = true
+
+    override def trafficFlows: Seq[TrafficFlow] = _trafficFlows.values.toList
+
+    override def isEnabled: Boolean = true
+
+    override def color: Color = Color.GREEN
+
+    override def extendColor(delta: Double): Unit = ???
+
+    override def trafficFlow(direction: Direction): TrafficFlow = _trafficFlows(direction)
+
+    override def setEnabled(enabled: Boolean): Unit = ???
+  }
+}
+
+
