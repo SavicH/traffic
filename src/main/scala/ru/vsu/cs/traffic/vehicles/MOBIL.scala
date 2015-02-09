cf1be687ddf4c37267e4275bdcce982cc8127d08
Sergey Savvin <sav1ch@hotmail.com>
2015-02-09 14:54:43 +0300

    Implement the lane method

--- src/main/scala/ru/vsu/cs/traffic/vehicles/MOBIL.scala
+++ src/main/scala/ru/vsu/cs/traffic/vehicles/MOBIL.scala
@@ -2,7 +2,7 @@ package ru.vsu.cs.traffic.vehicles
 
 import ru.vsu.cs.traffic.Vehicle
 
-class MOBIL (vehicle: Vehicle) {
+class MOBIL (private val vehicle: Vehicle) {
 
   private val p = vehicle.politenessFactor
 
@@ -10,7 +10,18 @@ class MOBIL (vehicle: Vehicle) {
 
   private val a = vehicle.thresholdAcceleration
 
-  def lane = vehicle.lane
+  private def safetyCriterion(lane: Int): Boolean = false
+
+  private def incentiveCriterion(lane: Int): Boolean = false
+
+  def lane: Int = {
+    List(vehicle.lane - 1, vehicle.lane + 1)
+      .filter(i => i > 0 && i < vehicle.trafficFlow.lanes)
+      .map(i => (safetyCriterion(i) && incentiveCriterion(i), i))
+      .find(_._1 == true)
+      .getOrElse((true, vehicle.lane))
+      ._2
+  }
 }
 
 object MOBIL {
