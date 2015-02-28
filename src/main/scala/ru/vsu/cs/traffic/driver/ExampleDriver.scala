c652d95db581546c6379686142df965f8b892a54
Sergey Savvin <sav1ch@hotmail.com>
2015-02-28 20:02:20 +0300

    Add ExampleDriver class

---
+++ src/main/scala/ru/vsu/cs/traffic/driver/ExampleDriver.scala
@@ -0,0 +1,17 @@
+package ru.vsu.cs.traffic.driver
+
+import ru.vsu.cs.traffic.{Point, TrafficModel}
+
+object ExampleDriver extends Driver {
+
+  override def model = {
+    val model = TrafficModel()
+    val prob = 0.4
+    val lanesCount = 2
+    model.addFlow(Point(200, 0), Point(200, 400), lanesCount, isOneWay = false, probability = _ => prob)
+    model.addFlow(Point(500, 0), Point(500, 400), lanesCount, isOneWay = false, probability = _ => prob)
+    model.addFlow(Point(0, 200), Point(750, 200), lanesCount, isOneWay = false, probability = _ => prob)
+    model.run()
+    model
+  }
+}
