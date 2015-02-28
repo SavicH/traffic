b651b75d9d8a2ccdc565f2f5f13b2bce293a1530
Sergey Savvin <sav1ch@hotmail.com>
2015-02-28 21:08:38 +0300

    Add traffic model events

--- src/main/scala/ru/vsu/cs/traffic/event/TrafficLightEvent.scala
+++ src/main/scala/ru/vsu/cs/traffic/event/TrafficLightEvent.scala
@@ -2,7 +2,7 @@ package ru.vsu.cs.traffic.event
 
 import ru.vsu.cs.traffic.TrafficLight
 
-trait TrafficLightEvent { }
+trait TrafficLightEvent
 
 case class ColorChanged(trafficLight: TrafficLight) extends TrafficLightEvent
 case class BeforeColorChanged(trafficLight: TrafficLight) extends TrafficLightEvent
