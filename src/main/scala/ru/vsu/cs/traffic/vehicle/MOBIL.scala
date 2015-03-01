9bbe18b28964059948676bee97ae544e05e97030
Sergey Savvin <sav1ch@hotmail.com>
2015-03-01 14:00:51 +0300

    Fix "kamikazes"

--- src/main/scala/ru/vsu/cs/traffic/vehicle/MOBIL.scala
+++ src/main/scala/ru/vsu/cs/traffic/vehicle/MOBIL.scala
@@ -1,5 +1,7 @@
 package ru.vsu.cs.traffic.vehicle
 
+import ru.vsu.cs.traffic.Vehicle
+
 class MOBIL(vehicle: MOBILVehicle) {
 
   private def p = vehicle.politenessFactor
@@ -8,16 +10,19 @@ class MOBIL(vehicle: MOBILVehicle) {
 
   private def a = vehicle.thresholdAcceleration
 
-  private def safetyCriterion(lane: Int): Boolean = {
-    vehicle.backVehicle(lane) match {
+  private def safetyCriterion(head: Vehicle, back: Vehicle): Boolean = {
+    val accelerationCriterion = back match {
       case _: VirtualVehicle => true
-      case v: IDMVehicle => (v.idm.acceleration(vehicle) > -b)
+      case v: IDMVehicle => v.idm.acceleration(vehicle) > -b
     }
+    accelerationCriterion &&
+      head.distance - vehicle.distance > head.length &&
+      vehicle.distance - back.distance > vehicle.length
   }
 
-  private def incentiveCriterion(lane: Int): Boolean = {
-    val left = vehicle.idm.acceleration(vehicle.headVehicle(lane)) - vehicle.idm.acceleration
-    val dis = vehicle.backVehicle(lane) match {
+  private def incentiveCriterion(head: Vehicle, back: Vehicle): Boolean = {
+    val left = vehicle.idm.acceleration(head) - vehicle.idm.acceleration
+    val dis = back match {
       case _: VirtualVehicle => 0
       case v: IDMVehicle => v.idm.acceleration - v.idm.acceleration(vehicle)
     }
@@ -28,7 +33,8 @@ class MOBIL(vehicle: MOBILVehicle) {
   def lane: Int = {
     List(vehicle.lane - 1, vehicle.lane + 1)
       .filter(i => i > 0 && i <= vehicle.trafficFlow.lanes)
-      .map(i => (safetyCriterion(i) && incentiveCriterion(i), i))
+      .map(i => (i, vehicle.headVehicle(i), vehicle.backVehicle(i)))
+      .map(i => (safetyCriterion(i._2, i._3) && incentiveCriterion(i._2, i._3), i._1))
       .find(_._1 == true)
       .getOrElse((true, vehicle.lane))
       ._2
